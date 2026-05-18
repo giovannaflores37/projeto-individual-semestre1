e7daf35e29e5044731a551e55c6547635b458f3d
giovannaflores37 <giovanna.flores@sptech.school>
2026-05-18 01:31:00 -0300

    Css e Html dos brawlers e ajuste do documento de conteúdos das paginas

---
+++ public/js/brawlers.js
@@ -0,0 +1,76 @@
+function mstDestruidor(){
+    document.getElementById("conteudo1").classList.remove("escondido");
+
+    document.getElementById("conteudo2").classList.add("escondido");
+    document.getElementById("conteudo3").classList.add("escondido");
+    document.getElementById("conteudo4").classList.add("escondido");
+    document.getElementById("conteudo5").classList.add("escondido");
+    document.getElementById("conteudo6").classList.add("escondido");
+    document.getElementById("conteudo7").classList.add("escondido");
+}
+
+function mstAlgoz(){
+    document.getElementById("conteudo2").classList.remove("escondido");
+
+    document.getElementById("conteudo1").classList.add("escondido");
+    document.getElementById("conteudo3").classList.add("escondido");
+    document.getElementById("conteudo4").classList.add("escondido");
+    document.getElementById("conteudo5").classList.add("escondido");
+    document.getElementById("conteudo6").classList.add("escondido");
+    document.getElementById("conteudo7").classList.add("escondido");
+}
+
+function mstTiroPreciso(){
+    document.getElementById("conteudo3").classList.remove("escondido");
+
+    document.getElementById("conteudo1").classList.add("escondido");
+    document.getElementById("conteudo2").classList.add("escondido");
+    document.getElementById("conteudo4").classList.add("escondido");
+    document.getElementById("conteudo5").classList.add("escondido");
+    document.getElementById("conteudo6").classList.add("escondido");
+    document.getElementById("conteudo7").classList.add("escondido");
+}
+
+function mstDetonador(){
+    document.getElementById("conteudo4").classList.remove("escondido");
+
+    document.getElementById("conteudo1").classList.add("escondido");
+    document.getElementById("conteudo2").classList.add("escondido");
+    document.getElementById("conteudo3").classList.add("escondido");
+    document.getElementById("conteudo5").classList.add("escondido");
+    document.getElementById("conteudo6").classList.add("escondido");
+    document.getElementById("conteudo7").classList.add("escondido");
+}
+
+function mstTanque(){
+    document.getElementById("conteudo5").classList.remove("escondido");
+
+    document.getElementById("conteudo1").classList.add("escondido");
+    document.getElementById("conteudo2").classList.add("escondido");
+    document.getElementById("conteudo3").classList.add("escondido");
+    document.getElementById("conteudo4").classList.add("escondido");
+    document.getElementById("conteudo6").classList.add("escondido");
+    document.getElementById("conteudo7").classList.add("escondido");
+}
+
+function mstSuporte(){
+    document.getElementById("conteudo6").classList.remove("escondido");
+
+    document.getElementById("conteudo1").classList.add("escondido");
+    document.getElementById("conteudo2").classList.add("escondido");
+    document.getElementById("conteudo3").classList.add("escondido");
+    document.getElementById("conteudo4").classList.add("escondido");
+    document.getElementById("conteudo5").classList.add("escondido");
+    document.getElementById("conteudo7").classList.add("escondido");
+}
+
+function mstControle(){
+    document.getElementById("conteudo7").classList.remove("escondido");
+
+    document.getElementById("conteudo1").classList.add("escondido");
+    document.getElementById("conteudo2").classList.add("escondido");
+    document.getElementById("conteudo3").classList.add("escondido");
+    document.getElementById("conteudo4").classList.add("escondido");
+    document.getElementById("conteudo5").classList.add("escondido");
+    document.getElementById("conteudo6").classList.add("escondido");
+}
